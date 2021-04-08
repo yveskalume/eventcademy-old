839d341b2aa02c29955febadd1200542e84542b4
YvesKalume <yvkalume@gmail.com>
2021-04-08 17:30:40 +0200

    home

--- app/src/main/java/com/yvkalume/eventcademy/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/yvkalume/eventcademy/ui/fragment/home/HomeFragment.kt
@@ -10,6 +10,7 @@ import com.yvkalume.eventcademy.EventBindingModel_
 import com.yvkalume.eventcademy.R
 import com.yvkalume.eventcademy.databinding.FragmentHomeBinding
 import com.yvkalume.eventcademy.featuredEvent
+import com.yvkalume.eventcademy.header
 import com.yvkalume.eventcademy.util.carousel
 import com.yvkalume.eventcademy.util.withModelsFrom
 
@@ -27,12 +28,22 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 id("featured")
             }
 
+            header {
+                id("online")
+                text("En Ligne")
+            }
+
             carousel {
                 id("carousel")
                 withModelsFrom(listOf(1,2,3,4,5)) {
                     EventBindingModel_()
                         .id(it)
                 }
+            }
+
+            header {
+                id("presentiel")
+                text("En Presentiel")
             }
 
             carousel {
