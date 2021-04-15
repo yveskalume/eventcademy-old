a70205ff74600b09fffed57b618475a7d6030fc2
YvesKalume <yvkalume@gmail.com>
2021-04-15 14:28:02 +0200

    eventRepository$getAll method

--- data/src/main/java/com/yvkalume/data/repository/EventRepositoryImpl.kt
+++ data/src/main/java/com/yvkalume/data/repository/EventRepositoryImpl.kt
@@ -1,14 +1,33 @@
 package com.yvkalume.data.repository
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.yvkalume.data.util.FireBasePath
 import com.yvkalume.domain.entity.Event
 import com.yvkalume.domain.repository.EventRepository
 import com.yvkalume.util.Result
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 
-class EventRepositoryImpl @Inject constructor() : EventRepository {
-    override fun getAll(): Flow<Result<List<Event>>> {
-        TODO("Not yet implemented")
+class EventRepositoryImpl @Inject constructor(private val firestore: FirebaseFirestore) : EventRepository {
+    @ExperimentalCoroutinesApi
+    override fun getAll() = callbackFlow {
+        firestore.collection(FireBasePath.events)
+            .addSnapshotListener { value, error ->
+                if (error != null && value == null) {
+                    if (!isClosedForSend) {
+                        offer(Result.Error(Exception("Une Erreur s'est produite")))
+                    }
+                    return@addSnapshotListener
+                }
+
+                value?.toObjects(Event::class.java)?.also {
+                    offer(Result.Success(it))
+                }
+            }
+        awaitClose()
     }
 
     override fun getOneByUid(uid: String): Flow<Result<Event>> {
