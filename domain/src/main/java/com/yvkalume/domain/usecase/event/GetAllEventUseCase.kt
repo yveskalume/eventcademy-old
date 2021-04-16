93b8f405d35fa6c20ab64dac5d93126681aa5b86
YvesKalume <yvkalume@gmail.com>
2021-04-16 16:53:39 +0200

    fix GetAllEventUseCase.kt

--- domain/src/main/java/com/yvkalume/domain/usecase/event/GetAllEventUseCase.kt
+++ domain/src/main/java/com/yvkalume/domain/usecase/event/GetAllEventUseCase.kt
@@ -5,11 +5,12 @@ import com.yvkalume.domain.repository.EventRepository
 import com.yvkalume.domain.util.FlowUseCase
 import com.yvkalume.util.Result
 import com.yvkalume.util.annotation.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.MainCoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetAllEventUseCase @Inject constructor (private val repository: EventRepository, @IoDispatcher dispatcher: MainCoroutineDispatcher) : FlowUseCase<Unit, List<Event>>(dispatcher) {
+class GetAllEventUseCase @Inject constructor (private val repository: EventRepository, @IoDispatcher dispatcher: CoroutineDispatcher) : FlowUseCase<Unit, List<Event>>(dispatcher) {
     override fun execute(parameters: Unit): Flow<Result<List<Event>>> {
         return repository.getAll()
     }
