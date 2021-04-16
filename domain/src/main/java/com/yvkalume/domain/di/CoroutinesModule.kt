964c77722aff3a9c1cd7b5181743b48322177dcb
YvesKalume <yvkalume@gmail.com>
2021-04-16 20:14:21 +0200

    update coroutines module

---
+++ domain/src/main/java/com/yvkalume/domain/di/CoroutinesModule.kt
@@ -0,0 +1,34 @@
+package com.yvkalume.domain.di
+
+import com.yvkalume.util.annotation.DefaultDispatcher
+import com.yvkalume.util.annotation.IoDispatcher
+import com.yvkalume.util.annotation.MainDispatcher
+import com.yvkalume.util.annotation.MainImmediateDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @MainImmediateDispatcher
+    @Provides
+    fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+}
