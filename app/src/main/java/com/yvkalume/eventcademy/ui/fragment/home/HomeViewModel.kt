5ac4f0c14dffe262ac961fb5a945cc4da5fd5495
YvesKalume <yvkalume@gmail.com>
2021-04-16 20:13:21 +0200

    homeviewmodel di

--- app/src/main/java/com/yvkalume/eventcademy/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/yvkalume/eventcademy/ui/fragment/home/HomeViewModel.kt
@@ -4,29 +4,31 @@ import com.airbnb.mvrx.MavericksViewModel
 import com.airbnb.mvrx.MavericksViewModelFactory
 import com.yvkalume.data.mapper.EventUiMapper
 import com.yvkalume.data.model.presentation.HomeData
-import com.yvkalume.domain.usecase.event.GetAllEventUseCase
 import com.yvkalume.domain.usecase.event.GetNextEventUseCase
-import com.yvkalume.domain.usecase.event.GetOfflineEvents
-import com.yvkalume.domain.usecase.event.GetOnlineEvents
+import com.yvkalume.domain.usecase.event.GetOfflineEventsUseCase
+import com.yvkalume.domain.usecase.event.GetOnlineEventsUseCase
 import com.yvkalume.eventcademy.di.mavericks.AssistedViewModelFactory
 import com.yvkalume.eventcademy.di.mavericks.hiltMavericksViewModelFactory
 import com.yvkalume.util.data
-import com.yvkalume.util.succeeded
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import java.lang.Exception
+import javax.inject.Singleton
 
-class HomeViewModel @AssistedInject constructor(
+class HomeViewModel @AssistedInject constructor (
     @Assisted state: HomeViewState,
-    private val getOnlineEvents: GetOnlineEvents,
-    private val getOfflineEvents: GetOfflineEvents,
+    private val getOnlineEvents: GetOnlineEventsUseCase,
+    private val getOfflineEvents: GetOfflineEventsUseCase,
     private val getNextEventUseCase: GetNextEventUseCase
 ) : MavericksViewModel<HomeViewState>(state) {
 
+
+    init {
+        getAllEvents()
+    }
 
     private fun getAllEvents() = viewModelScope.launch {
         val nextEvent =  getNextEventUseCase(Unit).map {
