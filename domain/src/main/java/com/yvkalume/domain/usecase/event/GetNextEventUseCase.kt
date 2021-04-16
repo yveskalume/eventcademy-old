d46d4fd4863fcc688ba4d0a0b4a784b2b1c6f1db
YvesKalume <yvkalume@gmail.com>
2021-04-16 12:13:02 +0200

    next event usecase

---
+++ domain/src/main/java/com/yvkalume/domain/usecase/event/GetNextEventUseCase.kt
@@ -0,0 +1,16 @@
+package com.yvkalume.domain.usecase.event
+
+import com.yvkalume.domain.entity.Event
+import com.yvkalume.domain.repository.EventRepository
+import com.yvkalume.domain.util.FlowUseCase
+import com.yvkalume.util.Result
+import com.yvkalume.util.annotation.IoDispatcher
+import kotlinx.coroutines.MainCoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetNextEventUseCase@Inject constructor (private val repository: EventRepository, @IoDispatcher dispatcher: MainCoroutineDispatcher) : FlowUseCase<Unit,Event>(dispatcher) {
+    override fun execute(parameters: Unit): Flow<Result<Event>> {
+        return  repository.getNext()
+    }
+}
