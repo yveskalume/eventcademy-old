147d2735fd718bdd25c94a9f8d0bc453f01b1fa7
YvesKalume <yvkalume@gmail.com>
2021-04-16 19:19:33 +0200

    initialize mvrx

--- app/src/main/java/com/yvkalume/eventcademy/app/App.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/App.kt
@@ -1,8 +1,13 @@
 package com.yvkalume.eventcademy.app
 
 import android.app.Application
+import com.airbnb.mvrx.Mavericks
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Mavericks.initialize(this)
+    }
 }
