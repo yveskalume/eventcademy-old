63bc8f2ee753fb775ca3c1194d0ee8b907fee466
YvesKalume <yvkalume@gmail.com>
2021-04-16 16:58:14 +0200

    fix GetNextEventUseCase.kt

--- domain/src/main/java/com/yvkalume/domain/usecase/event/GetNextEventUseCase.kt
+++ domain/src/main/java/com/yvkalume/domain/usecase/event/GetNextEventUseCase.kt
@@ -5,11 +5,12 @@ import com.yvkalume.domain.repository.EventRepository
 import com.yvkalume.domain.util.FlowUseCase
 import com.yvkalume.util.Result
 import com.yvkalume.util.annotation.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.MainCoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetNextEventUseCase@Inject constructor (private val repository: EventRepository, @IoDispatcher dispatcher: MainCoroutineDispatcher) : FlowUseCase<Unit,Event>(dispatcher) {
+class GetNextEventUseCase@Inject constructor (private val repository: EventRepository, @IoDispatcher dispatcher: CoroutineDispatcher) : FlowUseCase<Unit,Event>(dispatcher) {
     override fun execute(parameters: Unit): Flow<Result<Event>> {
         return  repository.getNext()
     }
