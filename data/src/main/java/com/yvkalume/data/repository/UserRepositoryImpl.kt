373a70c2528957d8bc5c15b6d770d2119cb4870c
YvesKalume <yvkalume@gmail.com>
2021-04-19 12:41:30 +0200

    user repository impl

--- data/src/main/java/com/yvkalume/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/yvkalume/data/repository/UserRepositoryImpl.kt
@@ -1,6 +1,12 @@
 package com.yvkalume.data.repository
 
+import com.yvkalume.domain.repository.UserRepository
+import com.yvkalume.util.Result
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class UserRepositoryImpl @Inject constructor() {
+class UserRepositoryImpl @Inject constructor() : UserRepository {
+    override fun signIn(): Flow<Result<Boolean>> {
+        TODO("Not yet implemented")
+    }
 }
