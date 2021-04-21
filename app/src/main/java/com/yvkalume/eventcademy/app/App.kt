4616f5ab248eff2f12307ac7159acaf80eb96c4e
YvesKalume <yvkalume@gmail.com>
2021-04-21 10:22:12 +0200

    add timber

--- app/src/main/java/com/yvkalume/eventcademy/app/App.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/App.kt
@@ -2,12 +2,17 @@ package com.yvkalume.eventcademy.app
 
 import android.app.Application
 import com.airbnb.mvrx.Mavericks
+import com.yvkalume.eventcademy.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Mavericks.initialize(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
