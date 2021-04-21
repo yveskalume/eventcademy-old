20178c33d7a202dcab3f1ba4f623325e6b838afb
YvesKalume <yvkalume@gmail.com>
2021-04-21 20:19:18 +0200

    format date

--- app/src/main/java/com/yvkalume/eventcademy/util/BindinUtil.kt
+++ app/src/main/java/com/yvkalume/eventcademy/util/BindinUtil.kt
@@ -1,9 +1,13 @@
 package com.yvkalume.eventcademy.util
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter(value = ["setImageUrl"], requireAll = false)
 fun ImageView.setImageUrl(url: String?) {
@@ -13,4 +17,12 @@ fun ImageView.setImageUrl(url: String?) {
             .apply(RequestOptions().placeholder(android.R.color.darker_gray))
             .into(this)
     }
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("humanDate")
+fun TextView.sethumanDate(eventDate: Date){
+    val date = SimpleDateFormat("dd/MM/yyyy",Locale.FRANCE).format(eventDate)
+    val time = SimpleDateFormat("HH:mm", Locale.FRANCE).format(eventDate)
+    text = "$date à $time"
 }
