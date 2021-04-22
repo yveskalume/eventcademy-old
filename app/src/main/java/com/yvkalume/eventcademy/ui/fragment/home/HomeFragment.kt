b4c6b37d133bf93f87dbd2e82bf120a98ef01cd1
YvesKalume <yvkalume@gmail.com>
2021-04-22 09:38:47 +0200

    fix: offline events clicklistener

--- app/src/main/java/com/yvkalume/eventcademy/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/yvkalume/eventcademy/ui/fragment/home/HomeFragment.kt
@@ -101,6 +101,10 @@ class HomeFragment : Fragment(R.layout.fragment_home), MavericksView {
                     EventBindingModel_()
                             .id(it.uid)
                             .event(it)
+                            .clickListener { _ ->
+                                val directions = HomeFragmentDirections.toEventFragment(it)
+                                findNavController().navigate(directions)
+                            }
                 }
             }
         }
