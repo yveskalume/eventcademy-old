27738c9d001148598c3e359982e1fffa110b5ae5
YvesKalume <19kn132@esisalama.org>
2022-02-01 13:01:51 +0200

    fix : RepositoryModule.kt

--- data/src/main/java/com/yvkalume/data/di/RepositoryModule.kt
+++ data/src/main/java/com/yvkalume/data/di/RepositoryModule.kt
@@ -1,13 +1,6 @@
 package com.yvkalume.data.di
 
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.yvkalume.data.repository.EventRepositoryImpl
-import com.yvkalume.data.repository.UserRepositoryImpl
-import com.yvkalume.domain.repository.EventRepository
-import com.yvkalume.domain.repository.UserRepository
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
@@ -16,18 +9,4 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
 
-    @Provides
-    fun provideEventRepository(firestore: FirebaseFirestore) : EventRepository {
-        return EventRepositoryImpl(firestore)
-    }
-
-    @Provides
-    fun provideUserRepository(firestore: FirebaseFirestore,firebaseAuth: FirebaseAuth) : UserRepository {
-        return UserRepositoryImpl(firestore,firebaseAuth)
-    }
-
-    @Provides
-    fun provideClubRepository(firestore: FirebaseFirestore) : ClubRepository {
-        return ClubRepositoryImpl(firestore)
-    }
 }
