bc6ac4fa3899180b942e8be13784d68b668def0e
YvesKalume <19kn132@esisalama.org>
2022-02-01 15:29:14 +0200

    fix : data module

--- data/src/main/java/com/yvkalume/data/util/Extension.kt
+++ data/src/main/java/com/yvkalume/data/util/Extension.kt
@@ -1,11 +1,2 @@
 package com.yvkalume.data.util
 
-import com.yvkalume.domain.entity.Event
-
-fun List<Event>.getOnlyFuture() : List<Event> {
-    sortedBy { it.date }
-    return filter { event ->
-        val now = System.currentTimeMillis()
-        return@filter  now <= event.date!!.time + 3600000
-    }
-}
