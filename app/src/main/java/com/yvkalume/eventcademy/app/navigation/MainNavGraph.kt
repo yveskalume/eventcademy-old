e7ab9228525becda61c5f1a3a81b66a9d0c365d7
YvesKalume <yvkalume@gmail.com>
2022-02-23 16:48:35 +0200

    bottom navigation

--- app/src/main/java/com/yvkalume/eventcademy/app/navigation/MainNavGraph.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/navigation/MainNavGraph.kt
@@ -1,22 +1,51 @@
 package com.yvkalume.eventcademy.app.navigation
 
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.yvkalume.eventcademy.ui.screen.agenda.AgendaScreen
 import com.yvkalume.eventcademy.ui.screen.eventdetail.EventDetailScreen
 import com.yvkalume.eventcademy.ui.screen.home.HomeScreen
 
 @Composable
 fun MainNavGraph() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.Home.route) {
-        composable(route = Screen.Home.route) {
-            HomeScreen(navController = navController)
-        }
+    val scaffoldState = rememberScaffoldState()
 
-        composable(route = Screen.EventDetails.route) {
-            EventDetailScreen(navController = navController)
+    Scaffold(
+        scaffoldState = scaffoldState,
+        bottomBar = {
+            BottomNavigation {
+                getBottomNavItems().forEach {
+                    BottomNavigationItem(
+                        selected = navController.currentDestination?.route == it.route,
+                        onClick = { navController.navigate(it.route) },
+                        icon = {
+                               Icon(imageVector = it.icon as ImageVector, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = it.label)
+                        }
+                    )
+                }
+            }
+        },
+        content = {
+            NavHost(navController = navController, startDestination = Screen.Home.route) {
+                composable(route = Screen.Home.route) {
+                    HomeScreen(navController = navController)
+                }
+
+                composable(route = Screen.EventDetails.route) {
+                    EventDetailScreen(navController = navController)
+                }
+                composable(route = Screen.Agenda.route) {
+                    AgendaScreen(navController = navController)
+                }
+            }
         }
-    }
+    )
 }
