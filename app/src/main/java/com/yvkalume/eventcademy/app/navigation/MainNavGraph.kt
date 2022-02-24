6a8de8c4029fa1510d83a7c9ad7af5447c72a3cb
YvesKalume <yvkalume@gmail.com>
2022-02-24 21:54:31 +0200

    bottom navigation

--- app/src/main/java/com/yvkalume/eventcademy/app/navigation/MainNavGraph.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/navigation/MainNavGraph.kt
@@ -1,10 +1,15 @@
 package com.yvkalume.eventcademy.app.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.yvkalume.eventcademy.ui.screen.agenda.AgendaScreen
 import com.yvkalume.eventcademy.ui.screen.eventdetail.EventDetailScreen
@@ -18,23 +23,27 @@ fun MainNavGraph() {
     Scaffold(
         scaffoldState = scaffoldState,
         bottomBar = {
-            BottomNavigation {
-                getBottomNavItems().forEach {
+            BottomNavigation(backgroundColor = MaterialTheme.colors.surface ) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                getBottomNavItems().forEach { screen ->
                     BottomNavigationItem(
-                        selected = navController.currentDestination?.route == it.route,
-                        onClick = { navController.navigate(it.route) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = { navController.navigate(screen.route) },
+                        selectedContentColor = MaterialTheme.colors.primary,
                         icon = {
-                               Icon(imageVector = it.icon as ImageVector, contentDescription = null)
+                               Icon(imageVector = screen.icon as ImageVector, contentDescription = null)
                         },
                         label = {
-                            Text(text = it.label)
+                            Text(text = screen.label)
                         }
                     )
                 }
             }
         },
-        content = {
-            NavHost(navController = navController, startDestination = Screen.Home.route) {
+        content = { contentPadding ->
+            NavHost(modifier = Modifier.padding(contentPadding),navController = navController, startDestination = Screen.Home.route) {
                 composable(route = Screen.Home.route) {
                     HomeScreen(navController = navController)
                 }
