f723aab5905ac488a7e10360052684acb5a579d4
YvesKalume <yvkalume@gmail.com>
2022-02-25 13:10:52 +0200

    save user in databse

--- domain/src/main/java/com/yvkalume/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/yvkalume/domain/repository/UserRepository.kt
@@ -1,11 +1,13 @@
 package com.yvkalume.domain.repository
 
+import com.yvkalume.domain.dto.CustomFirebaseUser
 import com.yvkalume.domain.entity.User
 import com.yvkalume.util.Result
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-    suspend fun signInWithGoogle(idToken: String) : Boolean
+    suspend fun signInWithGoogle(idToken: String) : CustomFirebaseUser
     suspend fun signInWithEmailAndPassword(email: String, password: String) : Boolean
     suspend fun signUpWithEmailAndPassword(email: String, name: String, password: String) : Boolean
+    suspend fun saveUserInDatabase(user: User) : Unit
 }
---
+++ domain/src/main/java/com/yvkalume/domain/usecase/user/SaveUserInDatabaseUseCase.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 EventCademy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yvkalume.domain.usecase.user
+
+import com.yvkalume.domain.entity.User
+import com.yvkalume.domain.repository.UserRepository
+import com.yvkalume.domain.util.CoroutineUseCase
+import com.yvkalume.util.annotation.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class SaveUserInDatabaseUseCase @Inject constructor(
+    private val repository: UserRepository,
+    @IoDispatcher dispatcher: CoroutineDispatcher
+) : CoroutineUseCase<User, Unit>(dispatcher) {
+    override suspend fun execute(params: User){
+        return repository.saveUserInDatabase(params)
+    }
+}
