6167a5a7e6d7a3cc3211a1c69ccc4ac31b2bc94e
YvesKalume <yvkalume@gmail.com>
2022-02-27 20:59:21 +0200

    setup App class

--- app/src/main/java/com/yvkalume/eventcademy/app/App.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/App.kt
@@ -1,11 +1,26 @@
 package com.yvkalume.eventcademy.app
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
 import com.airbnb.mvrx.Mavericks
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Mavericks.initialize(this)
+    }
+
+    companion object {
+        fun restart(context: Context) {
+            context.packageManager.getLaunchIntentForPackage(context.packageName)?.apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                addCategory(Intent.CATEGORY_HOME)
+                context.startActivity(this)
+            }
+        }
     }
 }
