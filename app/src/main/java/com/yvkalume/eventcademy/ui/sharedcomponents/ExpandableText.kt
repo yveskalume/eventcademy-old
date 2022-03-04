8da6b72f9d14e90d76aaf7e2b11e1644f9b2a826
YvesKalume <yvkalume@gmail.com>
2022-03-04 11:25:22 +0200

    expandable text

---
+++ app/src/main/java/com/yvkalume/eventcademy/ui/sharedcomponents/ExpandableText.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2022 EventCademy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yvkalume.eventcademy.ui.sharedcomponents
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntSize
+
+@Composable
+fun ExpandableText(
+    text: String,
+    modifier: Modifier = Modifier,
+    minimizedMaxLines: Int = 1,
+) {
+    var cutText by remember(text) { mutableStateOf<String?>(null) }
+    var expanded by remember { mutableStateOf(false) }
+    val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
+    val seeMoreSizeState = remember { mutableStateOf<IntSize?>(null) }
+    val seeMoreOffsetState = remember { mutableStateOf<Offset?>(null) }
+
+    // getting raw values for smart cast
+    val textLayoutResult = textLayoutResultState.value
+    val seeMoreSize = seeMoreSizeState.value
+    val seeMoreOffset = seeMoreOffsetState.value
+
+    LaunchedEffect(text, expanded, textLayoutResult, seeMoreSize) {
+        val lastLineIndex = minimizedMaxLines - 1
+        if (!expanded && textLayoutResult != null && seeMoreSize != null
+            && lastLineIndex + 1 == textLayoutResult.lineCount
+            && textLayoutResult.isLineEllipsized(lastLineIndex)
+        ) {
+            var lastCharIndex = textLayoutResult.getLineEnd(lastLineIndex, visibleEnd = true) + 1
+            var charRect: Rect
+            do {
+                lastCharIndex -= 1
+                charRect = textLayoutResult.getCursorRect(lastCharIndex)
+            } while (
+                charRect.left > textLayoutResult.size.width - seeMoreSize.width
+            )
+            seeMoreOffsetState.value = Offset(charRect.left, charRect.bottom - seeMoreSize.height)
+            cutText = text.substring(startIndex = 0, endIndex = lastCharIndex)
+        }
+    }
+
+    Box(modifier) {
+        Text(
+            text = cutText ?: text,
+            maxLines = if (expanded) Int.MAX_VALUE else minimizedMaxLines,
+            overflow = TextOverflow.Ellipsis,
+            onTextLayout = { textLayoutResultState.value = it },
+        )
+        if (!expanded) {
+            val density = LocalDensity.current
+            Text(
+                "... See more",
+                onTextLayout = { seeMoreSizeState.value = it.size },
+                modifier = Modifier
+                    .then(
+                        if (seeMoreOffset != null)
+                            Modifier.offset(
+                                x = with(density) { seeMoreOffset.x.toDp() },
+                                y = with(density) { seeMoreOffset.y.toDp() },
+                            )
+                        else
+                            Modifier
+                    )
+                    .clickable {
+                        expanded = true
+                        cutText = null
+                    }
+                    .alpha(if (seeMoreOffset != null) 1f else 0f)
+            )
+        }
+    }
+}
