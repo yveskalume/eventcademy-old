0bbe5ef38ca0962609f33e7c00a20f61ff5338f5
YvesKalume <yvkalume@gmail.com>
2022-03-04 11:26:32 +0200

    remove unnecessary screens

--- app/src/main/java/com/yvkalume/eventcademy/app/navigation/Screen.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/navigation/Screen.kt
@@ -1,15 +1,16 @@
 package com.yvkalume.eventcademy.app.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.rounded.DateRange
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(val route: String, val label: String = "", val icon: ImageVector? = null) {
     object MainNavHost : Screen(route = "main-navhost")
     object Splash : Screen(route = "splash")
     object Login : Screen(route = "login")
-    object Home : Screen(route = "home", label = "Accueil", icon = Icons.Default.Home)
+    object Home : Screen(route = "home", label = "Accueil", icon = Icons.Rounded.Home)
     object Agenda : Screen(route = "Agenda", label = "Agenda", icon = Icons.Rounded.DateRange)
     object EventDetails : Screen(route = "event-details")
 }
--- app/src/main/java/com/yvkalume/eventcademy/ui/screen/notification/NotificationScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.yvkalume.eventcademy.ui.screen.notification
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun NotificationScreen() {
-
-}
