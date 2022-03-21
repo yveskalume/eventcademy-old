b06d27bbf8215d67212d5473deca74375fe950d1
YvesKalume <yvkalume@gmail.com>
2022-03-21 20:39:36 +0200

    remove bottom nav

--- app/src/main/java/com/yvkalume/eventcademy/app/navigation/MainNavGraph.kt
+++ app/src/main/java/com/yvkalume/eventcademy/app/navigation/MainNavGraph.kt
@@ -22,26 +22,26 @@ fun MainNavGraph() {
 
     Scaffold(
         scaffoldState = scaffoldState,
-        bottomBar = {
-            BottomNavigation(backgroundColor = MaterialTheme.colors.surface ) {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
-
-                getBottomNavItems().forEach { screen ->
-                    BottomNavigationItem(
-                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                        onClick = { navController.navigate(screen.route) },
-                        selectedContentColor = MaterialTheme.colors.primary,
-                        icon = {
-                               Icon(imageVector = screen.icon as ImageVector, contentDescription = null)
-                        },
-                        label = {
-                            Text(text = screen.label)
-                        }
-                    )
-                }
-            }
-        },
+//        bottomBar = {
+//            BottomNavigation(backgroundColor = MaterialTheme.colors.surface ) {
+//                val navBackStackEntry by navController.currentBackStackEntryAsState()
+//                val currentDestination = navBackStackEntry?.destination
+//
+//                getBottomNavItems().forEach { screen ->
+//                    BottomNavigationItem(
+//                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+//                        onClick = { navController.navigate(screen.route) },
+//                        selectedContentColor = MaterialTheme.colors.primary,
+//                        icon = {
+//                               Icon(imageVector = screen.icon as ImageVector, contentDescription = null)
+//                        },
+//                        label = {
+//                            Text(text = screen.label)
+//                        }
+//                    )
+//                }
+//            }
+//        },
         content = { contentPadding ->
             NavHost(modifier = Modifier.padding(contentPadding),navController = navController, startDestination = Screen.Home.route) {
                 composable(route = Screen.Home.route) {
